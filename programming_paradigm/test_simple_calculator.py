9ce2f44986655102d6432c7c6a1a808c2ea0f68c
Anthony Nyanumba <nyanumbaanthony@gmail.com>
2025-08-02 06:08:02 +0300

    Fixed the test_divide name

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -27,7 +27,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.multiply(0, 5), 0)
         self.assertEqual(self.calc.multiply(-2, -2), 4)
 
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertEqual(self.calc.divide(5, -1), -5)
         self.assertEqual(self.calc.divide(0, 5), 0)
